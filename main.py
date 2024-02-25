1f507fda6fda61fc22e72e75a2d9078a6b13f0db
mayyas-nakhli <mayyasnakhli@gmail.com>
2024-02-25 21:50:31 +0400

    User login

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from fastapi import FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from datetime import datetime, timedelta, timezone
+from fastapi import FastAPI, HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy import create_engine, URL, text, MetaData
 from sqlalchemy import Table, Integer, Column, String, TIMESTAMP
-from sqlalchemy.sql import insert, select
+from sqlalchemy.sql import insert, select, literal_column
 from sqlalchemy.exc import IntegrityError
 from dotenv import load_dotenv
 from os import getenv
 from pydantic import BaseModel, EmailStr
-from datetime import datetime
-from typing import Annotated
+from typing import Annotated, Union
 from passlib.context import CryptContext
+from jose import JWTError, jwt
 
 
 app = FastAPI()
@@ -36,6 +37,9 @@ DB_USER = getenv("DB_USER")
 DB_PASSWORD = getenv("DB_PASSWORD")
 DB_PORT = getenv("DB_PORT")
 DB_NAME = getenv("DB_NAME")
+ACCESS_TOKEN_EXPIRE_MINUTES = getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+ALGORITHM = getenv("ALGORITHM")
+SECRET_KEY = getenv("SECRET_KEY")
 
 DB_URL = URL.create(
     "postgresql+pg8000",
@@ -66,6 +70,15 @@ class UserOut(UserBase):
     updated_at: datetime | None = None
 
 
+class UserInDatabase(UserOut):
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
 ouath2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
 
@@ -74,9 +87,54 @@ def get_password_hash(password) -> str:
     return pwd_context.hash(password)
 
 
-@app.get("/welcome")
-async def welcome():
-    return {"message": "hi there"}
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_user(username: str) -> UserInDatabase:
+    select_stmt = select(users_table).where(users_table.c.username == username)
+    with engine.connect() as conn:
+        try:
+            result = conn.execute(select_stmt)
+            user_data = result.fetchone()._asdict()
+            if user_data.keys() == 0:
+                raise Exception("User not found")
+            return user_data
+        except Exception as e:
+            print(e)
+            return {}
+
+
+def get_user_public_data(user: UserInDatabase) -> UserOut:
+    del user['password']
+    return user
+
+
+def get_authenticated_user(
+        username: str,
+        password: str
+) -> Union[UserOut, False]:
+    user = get_user(username)
+    if not user:
+        return False
+    if not verify_password(password, user['password']):
+        return False
+    user_data = get_user_public_data(user)
+    return user_data
+
+
+def create_access_token(
+        data: dict,
+        expires_delta: Union[timedelta, None] = None
+):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 
 @app.post("/register")
@@ -88,19 +146,18 @@ async def register(user: UserIn):
         password=hashed_password,
         first_name=user.first_name,
         last_name=user.last_name,
-    )
+    ).returning(literal_column('*'))
 
     with engine.connect() as conn:
         try:
             result = conn.execute(insert_stmt)
             conn.commit()
-            select_stmt = select(users_table).where(
-                users_table.c.id == result.inserted_primary_key[0]
-            )
-            select_result = conn.execute(select_stmt)
-            user_data = select_result.fetchone()._asdict()
-            del user_data['password']
-            return {"message": "User created successfully", "user": user_data}
+            user_data = result.fetchone()._asdict()
+            response_user = get_user_public_data(user_data)
+            return {
+                "message": "User created successfully",
+                "user": response_user
+            }
         except IntegrityError as e:
             if "unique constraint" in str(e):
                 raise HTTPException(
@@ -110,3 +167,21 @@ async def register(user: UserIn):
                 raise HTTPException(
                     status_code=500, detail="Database error."
                 )
+
+
+@app.post("/login")
+async def login(
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+) -> Token:
+    user = get_authenticated_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
+    access_token = create_access_token(
+        data={"sub": user['username']}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
